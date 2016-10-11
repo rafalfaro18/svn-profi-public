dc553204117a28ea03495b9105df45cb86662f34
rafalfaro18 <rafalfaro_18@outlook.com>
2016-10-11 17:49:41 -0600

    Update svn-profi-public.php

--- svn-profi-public.php
+++ svn-profi-public.php
@@ -3,9 +3,12 @@
 * Plugin Name: Cool Plugin
 * Plugin URI: https://github.com/rafalfaro18/svn-profi-public
 * Description: Does Nothing
-* Version: 1.0.0
+* Version: 2.0.0
 * Author: @rafalfaro_18
 * Author URI: http://rafalfaro.com
 * License: GNU GENERAL PUBLIC LICENSE
 */
+
+echo "<p>hola</p>";
+
 ?>

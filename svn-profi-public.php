072cc5534d1d235af277c18463ef76bca1e1e160
rafalfaro18 <rafalfaro_18@outlook.com>
2016-10-11 17:40:16 -0600

    Update and rename hola-mundo.php to svn-profi-public.php

---
+++ svn-profi-public.php
@@ -0,0 +1,11 @@
+<?php 
+/**
+* Plugin Name: Cool Plugin
+* Plugin URI: https://github.com/rafalfaro18/svn-profi-public
+* Description: Does Nothing
+* Version: 1.0.0
+* Author: @rafalfaro_18
+* Author URI: http://rafalfaro.com
+* License: GNU GENERAL PUBLIC LICENSE
+*/
+?>

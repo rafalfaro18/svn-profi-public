430c454a5493d574950b26a2a382041dc9391aa0
Rafael Alfaro <rafalfaro_18@outlook.com>
2016-10-12 08:36:49 -0600

    ffff
    Signed-off-by: Rafael Alfaro <rafalfaro_18@outlook.com>

--- svn-profi-public.php
+++ svn-profi-public.php
@@ -3,7 +3,7 @@
 * Plugin Name: Cool Plugin
 * Plugin URI: https://github.com/rafalfaro18/svn-profi-public
 * Description: Does Nothing
-* Version: 1.0.2
+* Version: 2.0.0
 * Author: @rafalfaro_18
 * Author URI: http://rafalfaro.com
 * License: GNU GENERAL PUBLIC LICENSE

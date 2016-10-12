a2b8c453e37b8c23f7e81c0f23e2095211b14700
Rafael Alfaro <rafalfaro_18@outlook.com>
2016-10-12 08:41:10 -0600

    fgd

--- svn-profi-public.php
+++ svn-profi-public.php
@@ -3,7 +3,7 @@
 * Plugin Name: Cool Plugin
 * Plugin URI: https://github.com/rafalfaro18/svn-profi-public
 * Description: Does Nothing
-* Version: 5.0.0
+* Version: 6.0.0
 * Author: @rafalfaro_18
 * Author URI: http://rafalfaro.com
 * License: GNU GENERAL PUBLIC LICENSE
